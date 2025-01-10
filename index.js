1d58c44f0da61f3f34b5ff0999ba10beedb73e2d
Milk-Cool <milan-pl@mail.ru>
2025-01-10 10:38:51 +0300

    v0.3.0: web archiver

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export * from "./publicdocs.js";
 export * from "./website.js";
 export * from "./hhru.js";
+export * from "./web.js";
---
+++ web.js
@@ -0,0 +1,51 @@
+import puppeteer from "puppeteer";
+
+export class WebArchiver {
+    /**
+     * @param {string[]} allowedHosts Allowed hostst
+     */
+    constructor(allowedHosts) {
+        this.allowedHosts = allowedHosts ?? ["letovo.ru", "letovo.site", "letovo.school"];
+    }
+
+    /**
+     * Initializes the object.
+     */
+    async init() {
+        this.browser = await puppeteer.launch();
+    }
+
+    /**
+     * Archives a page
+     * 
+     * @param {string} url The URL
+     * @returns {null | string} MHTML
+     */
+    async archiveToMHTML(url) {
+        const urlParsed = new URL(url);
+        if(!this.allowedHosts.includes(
+            urlParsed.host.split(".").reverse().slice(0, 2).reverse().join(".")
+        )) return null;
+        let data = null, page;
+        try {
+            page = await this.browser.newPage();
+            await page.goto(url);
+            await page.waitForNetworkIdle();
+
+            const client = await page.createCDPSession();
+            ({ data } = await client.send("Page.captureSnapshot", {}));
+        } catch(e) {
+            console.error(e);
+        } finally {
+            await page?.close();
+            return data;
+        }
+    }
+
+    /**
+     * Deinitializes the object.
+     */
+    async deinit() {
+        await this.browser.close();
+    }
+};
