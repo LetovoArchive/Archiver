40bc801029dceb7c7df1a78882c8c9fdb04b62d3
Milk-Cool <coder.tomsk@gmail.com>
2025-10-21 13:19:17 +0300

    library archival

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * from "./publicdocs.js";
 export * from "./website.js";
 export * from "./hhru.js";
 export * from "./web.js";
+export * from "./library.js";
---
+++ library.js
@@ -0,0 +1,64 @@
+import Parser from "rss-parser";
+
+/** @typedef {{ title: string, identifier: string, link: string }} ArchivedItem */
+/** @typedef {ArchivedItem[]} ArchivedItems */
+
+const BRANCHES = ["LET", "LETJR"];
+const getURL = (branch, offset) => `https://library.letovo.ru/cgi-bin/koha/opac-search.pl?&limit=branch%3A${branch}&format=rss&sort_by=title_az&offset=${offset}`;
+export class LibraryArchiver {
+    /**
+     * Initializes a LibraryArchiver.
+     * @param {string} username Letovo username
+     * @param {string} password Letovo password
+     */
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Archives all items in the Letovo library.
+     * @returns {Promise<ArchivedItems>} All items
+     */
+    async archiveLibrary() {
+        const f1 = await fetch("https://library.letovo.ru");
+        const cookie = f1.headers.getSetCookie().find(x => x.startsWith("CGISESSID=")).split(";")[0];
+        const f2 = await fetch("https://library.letovo.ru/cgi-bin/koha/opac-main.pl", {
+            method: "POST",
+            body: new URLSearchParams({
+                koha_login_context: "opac",
+                userid: this.username,
+                password: this.password
+            }).toString(),
+            headers: {
+                "content-type": "application/x-www-form-urlencoded",
+                cookie
+            }
+        });
+        console.log(await f2.text());
+
+        const parser = new Parser({
+            customFields: {
+                item: ["title", "dc:identifier", "link"]
+            },
+            headers: {
+                cookie
+            }
+        });
+
+        /** @type {ArchivedItems} */
+        const items = [];
+        for(const branch of BRANCHES) {
+            let offset = 0;
+            while(true) {
+                const feed = await parser.parseURL(getURL(branch, offset));
+                for(const item of feed.items) {
+                    items.push({ title: item.title.trim(), identifier: item["dc:identifier"], link: item.link });
+                    console.log(items.at(-1));
+                }
+                offset += feed.items.length;
+            }
+        }
+        return items.sort((a, b) => ("" + a.title).localeCompare(b.title));
+    }
+}
